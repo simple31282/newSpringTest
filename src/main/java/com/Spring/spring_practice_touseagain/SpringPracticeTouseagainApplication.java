b5efa5fe8d7975b0f0e0305af8acb548cdff8fd0
Akin Onisile <69411318+simple31282@users.noreply.github.com>
2022-06-30 19:07:28 -0400

    Add files via upload

---
+++ src/main/java/com/Spring/spring_practice_touseagain/SpringPracticeTouseagainApplication.java
@@ -0,0 +1,13 @@
+package com.Spring.spring_practice_touseagain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPracticeTouseagainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPracticeTouseagainApplication.class, args);
+	}
+
+}
